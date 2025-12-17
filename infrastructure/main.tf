4adaceabef53dea35c1d4c55e138ce07561aa788
tysker <joergoertel@proton.me>
2025-12-17 12:00:22 +0100

    infra(terraform): add private app server

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,3 +6,12 @@ resource "linode_instance" "jump" {
 
   private_ip = true
 }
+
+resource "linode_instance" "app" {
+  label  = "cloud-devops-app"
+  region = var.region
+  type   = var.instance_type
+  image  = "linode/ubuntu22.04"
+
+  private_ip = true
+}
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -7,3 +7,8 @@ output "jump_private_ip" {
   description = "Private IPv4 address of the jump server"
   value       = linode_instance.jump.private_ip_address
 }
+
+output "app_private_ip" {
+  description = "Private IPv4 address of the application server"
+  value       = linode_instance.app.private_ip_address
+}
